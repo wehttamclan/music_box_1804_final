3e98b1279834d96ae9d71b0f1e27f31b0c6e7b71
Dione Wilson <dionew1@gmail.com>
2018-07-06 10:39:09 -0600

    Completed Model, Migrations and Database lesson

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -4,4 +4,8 @@ describe Artist, type: :model do
   describe 'validations' do
     it {should validate_presence_of(:name)}
   end
+
+  describe 'relationships' do
+    it {should have_many(:songs)}
+  end
 end
