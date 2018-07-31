d5bbfa3c47ff1727ddbfe60b93f3f4e59ede6431
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-07-31 10:38:08 -0600

    adds average rating to genre show page

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,5 +3,7 @@ class Genre < ApplicationRecord
   has_many :song_genres
   has_many :songs, through: :song_genres
 
-
+  def average_rating
+    songs.average(:rating)
+  end
 end
--- spec/features/visitor_visits_genre_show_page_spec.rb
+++ spec/features/visitor_visits_genre_show_page_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe 'a visitor visits genre index page' do
-  it "sees all genres" do
+describe 'a visitor visits genre show page' do
+  it "sees all songs associated with the genre" do
     artist = Artist.create(name: 'Journey')
     song_1 = artist.songs.create(title: "Don't Stop Believing", length: 320, play_count: 390808)
     song_2 = artist.songs.create(title: "Anyway You Want It", length: 420, play_count: 67908)
@@ -17,5 +17,20 @@ describe 'a visitor visits genre index page' do
     expect(page).to have_content(song_1.title)
     expect(page).to have_content(song_2.title)
     expect(page).to_not have_content(song_3.title)
+  end
+  it "sees all songs associated with the genre" do
+    artist = Artist.create(name: 'Journey')
+    song_1 = artist.songs.create(title: "Don't Stop Believing", length: 320, play_count: 390808, rating: 1)
+    song_2 = artist.songs.create(title: "Anyway You Want It", length: 420, play_count: 67908, rating: 3)
+    song_3 = artist.songs.create(title: "not a song", length: 123, play_count: 122, rating: 5)
+    genre_1 = Genre.create(name:'Rock')
+    SongGenre.create(song_id: song_1.id, genre_id: genre_1.id)
+    SongGenre.create(song_id: song_2.id, genre_id: genre_1.id)
+    SongGenre.create(song_id: song_3.id, genre_id: genre_1.id)
+
+
+    visit genre_path(genre_1)
+
+    expect(page).to have_content(genre_1.average_rating)
   end
 end
